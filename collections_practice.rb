bf3870a3f9d2193c4802bdaa480a4e22bccecf2f
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-08-30 08:28:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -42,5 +42,14 @@ def sort_array_desc (array)
  end
  
  def add_s (array)
+   array.each_with_index.collect do |word,index|
+     if index != 1
+       word << "s"
+     else
+       "feet"
+     end
+     array
+     end
+       
    
  end
